7a2b2931fb23587a072a615ac26c693abcf7d3a4
ohh21743-cell <ohh21743@gmail.com>
2026-06-09 16:28:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.post('/v1/chat/completions', async (req, res) => {
       model: nimModel,
       messages: messages,
       temperature: temperature || 0.75,
-      max_tokens: max_tokens || 9024,
+      max_tokens: max_tokens || 16384,
       extra_body: ENABLE_THINKING_MODE ? { chat_template_kwargs: { thinking: true } } : undefined,
       stream: stream || false
     };
